9ccc27a2eb4530b2611f956d0d2bc15ce3304b82
ariefkahfi <ariefkahfi@gmal.com>
2018-10-15 13:18:28 +0700

    update daftar.php

---
+++ backend/daftar.php
@@ -0,0 +1,11 @@
+<?php
+
+     
+    
+$username = $_POST['username'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$lahir = $_POST['lahir'];
+$no_hp = $_POST['no_hp'];
+
